4f6826361b3f26bc1ee2fcafeede9fa9dbc2d482
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-05-12 11:02:46 -0700

    added ball collision detection

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -15,16 +15,35 @@ export default class Ball {
 		this.x = this.boardWidth/2;
 		this.y = this.boardHeight/2;
 
-		// Generate a random number between -5 and 5
-		this.vy = Math.floor(Math.random() * 10 - 5); 
+		// Generate a random number between -5 and 5 that's not 0
+		this.vy = 0;
+
+		while( this.vy === 0 ) {
+			this.vy = Math.floor(Math.random() * 10 - 5);	
+		}
 
 		// A number between -5 and 5, based on the vy
 		this.vx = this.direction * (6 - Math.abs(this.vy));
 	}
 
+	wallCollision() {
+		const hitLeft  = this.x - this.radius <= 0;
+		const hitRight = this.x + this.radius >= this.boardWidth;
+		const hitTop = this.y - this.radius <= 0;
+		const hitBottom = this.y + this.radius >= this.boardHeight;
+
+		if (hitLeft || hitRight) {
+			this.vx = -this.vx;
+		} else if (hitTop || hitBottom) {
+			this.vy = -this.vy;
+		}
+	}
+
 	render(svg) {
 		this.x += this.vx;
 		this.y += this.vy;
+
+		this.wallCollision();
 
 		let ball = document.createElementNS(SVG_NS, 'circle');
 		ball.setAttributeNS(null, 'cx', this.x);
@@ -32,6 +51,6 @@ export default class Ball {
 		ball.setAttributeNS(null, 'r', this.radius);
 		ball.setAttributeNS(null, 'fill', '#FFF');
 
-		svg.appendChild(ball);				
+		svg.appendChild(ball);
   }
 }
